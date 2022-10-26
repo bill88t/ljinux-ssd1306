48041a4e3e98a1a4c67740d53c5bbd985be3f946
Bill Sideris <bill88t@feline.gr>
2022-10-26 14:14:42 +0300

    move font, and update install scripts

--- files/installer.py
+++ files/installer.py
@@ -1,17 +1,7 @@
-ljinux.api.var("argj", f"cp display.lja /bin/display.lja")
-ljinux.based.command.fpexecc([None, "/bin/cp.py"])
-ljinux.api.var("argj", f"cp display.py /bin/display.py")
-ljinux.based.command.fpexecc([None, "/bin/cp.py"])
 ljinux.api.var("argj", f"cp ssd1306.mpy &/lib/drivers/ssd1306.mpy")
 ljinux.based.command.fpexecc([None, "/bin/cp.py"])
-ljinux.api.var("argj", f"cp font5x8.bin &/font5x8.bin")
-ljinux.based.command.fpexecc([None, "/bin/cp.py"])
 
-for filee in [
-    "adafruit_framebuf.mpy",
-    "adafruit_ssd1306.mpy",
-]:
-    ljinux.api.var("argj", f"cp {filee} &/lib/{filee}")
-    ljinux.based.command.fpexecc([None, "/bin/cp.py"])
+ljinux.api.var("argj", f"cp adafruit_ssd1306.mpy &/lib/adafruit_ssd1306.mpy")
+ljinux.based.command.fpexecc([None, "/bin/cp.py"])
 
 ljinux.api.var("return", "0")
--- files/uninstaller.py
+++ files/uninstaller.py
@@ -1,7 +1,4 @@
-ljinux.api.var(
-    "argj",
-    "a &/lib/drivers/ssd1306.mpy &/font5x8.bin &/lib/adafruit_framebuf.mpy &/lib/adafruit_ssd1306.mpy",
-)
+ljinux.api.var("argj", "a &/lib/drivers/ssd1306.mpy &/lib/adafruit_ssd1306.mpy")
 ljinux.based.command.fpexecc([None, "/bin/rm.py"])
 
 ljinux.api.var("return", "0")
