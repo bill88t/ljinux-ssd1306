23a0d93f881d7d089496d476ccd523ebf667d577
Bill Sideris <bill88t@feline.gr>
2022-10-27 14:34:28 +0300

    remove uneccessary fstrings

--- files/installer.py
+++ files/installer.py
@@ -1,7 +1,7 @@
-ljinux.api.var("argj", f"cp ssd1306.mpy &/lib/drivers/ssd1306.mpy")
+ljinux.api.var("argj", "cp ssd1306.mpy &/lib/drivers/ssd1306.mpy")
 ljinux.based.command.fpexecc([None, "/bin/cp.py"])
 
-ljinux.api.var("argj", f"cp adafruit_ssd1306.mpy &/lib/adafruit_ssd1306.mpy")
+ljinux.api.var("argj", "cp adafruit_ssd1306.mpy &/lib/adafruit_ssd1306.mpy")
 ljinux.based.command.fpexecc([None, "/bin/cp.py"])
 
 ljinux.api.var("return", "0")
